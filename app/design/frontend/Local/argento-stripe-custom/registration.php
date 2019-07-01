6389edabc83cefc447ea74256e27053dbbbcd39f
charmming <charmming4u@gmail.com>
2019-07-01 10:48:20 +0100

    added new theme layout

---
+++ app/design/frontend/Local/argento-stripe-custom/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Local/argento-stripes-custom',
+    __DIR__
+);
